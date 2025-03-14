acdd600e2801b2601face81e5e79d263d80d7ec5
Roy Jones <royjones.sena@gmail.com>
2025-03-14 20:20:33 -0300

    alterando o banco para h2

--- cine-app/src/main/java/br/com/cinetech/dao/MovieDao.java
+++ cine-app/src/main/java/br/com/cinetech/dao/MovieDao.java
@@ -7,18 +7,18 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class MovieDao {
+
     public void CreateMovie(Movie movie) {
 
         String SQL = "INSERT INTO movie(name) VALUES(?)";
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinetech?user=root&password=root");
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa","sa");
 
             System.out.println("Sucess in databsae connection");
 
@@ -37,41 +37,41 @@ public class MovieDao {
         }
 
     }
+
+    public List<Movie> findAllMovies() {
+
+
+        String sql = "SELECT * FROM MOVIE";
+
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+            System.out.println("Sucess in databsae connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Movie>  moveis = new ArrayList<>();
+
+            while (resultSet.next()) {
+
+                String moviename = resultSet.getString("name");
+
+                Movie movie = new Movie(moviename);
+
+                moveis.add(movie);
+            }
+            System.out.println("success in select * movies");
+
+            connection.close();
+            return moveis;
+
+        } catch (Exception e) {
+
+            System.out.println("falha ao conncetar no databsae");
+
+            return Collections.emptyList();
+        }
+
+    }
 }
-//    public List<Movie> findAllMovies() {
-//
-//
-//        String sql = "SELECT * FROM MOVIE";
-//
-//
-//        try {
-//            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
-//            System.out.println("Sucess in databsae connection");
-//
-//            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//
-//            List<Movie>  moveis = new ArrayList<>();
-//
-//            while (resultSet.next()) {
-//
-//                String moviename = resultSet.getString("name");
-//
-//                Movie movie = new Movie(moviename);
-//
-//                moveis.add(movie);
-//            }
-//            System.out.println("success in select * movies");
-//
-//            connection.close();
-//            return moveis;
-//
-//        } catch (Exception e) {
-//
-//            System.out.println("falha ao conncetar no databsae");
-//
-//            return Collections.emptyList();
-//        }
-//
-//    }
-//}
